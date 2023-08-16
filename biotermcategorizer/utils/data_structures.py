f68f12713330b9478904008c5749f054e0d309be
sergimarsol <smarsoto7@alumnes.ub.edu>
2023-08-16 12:32:37 +0200

    Change Keyword class

--- biotermcategorizer/utils/data_structures.py
+++ biotermcategorizer/utils/data_structures.py
@@ -4,3 +4,15 @@ class Keyword:
         self.method = method
         self.score = score
         self.span = [ini, fin]
+
+    def __repr__(self):
+        """
+        A method to return a string representation of the Keyword object.
+
+        Parameters:
+        - None
+
+        Returns:
+        - A string representing the Keyword object.
+        """
+        return f"<Keyword(text='{self.text}', method='{self.method}', score='{self.score}', span='{self.span}')>"
